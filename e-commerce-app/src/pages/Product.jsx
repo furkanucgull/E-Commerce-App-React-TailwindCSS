269f4d6d6a57e21a3981425ba6551fb0151ef324
furkanucgull <furkanucgull@gmail.com>
2024-07-12 14:45:46 +0200

    css-features

--- e-commerce-app/src/pages/Product.jsx
+++ e-commerce-app/src/pages/Product.jsx
@@ -6,10 +6,10 @@ function Product({ product }) {
   const navigate = useNavigate();
   return (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
-      <a className="flex items-center justify-center " href="#">
+      <a className="flex items-center justify-center bg-white " href="#">
         <img className="p-8 w-48 h-40 object-contain object-center rounded-t-lg" src={image} alt="product image" />
       </a>
-      <div className="px-5 pb-5 h-48">
+      <div className="px-5 pb-5 h-48 bg-white">
         <a>
           <h5 className="text-base font-semibold tracking-tight text-gray-900 dark:text-white max-h-14">
             {title.substring(0, 20)}
